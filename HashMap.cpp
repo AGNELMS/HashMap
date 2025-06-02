e79d2a529c33d5eb10b410c52dfd8d40811de84f
AGNELMS <agnelms27@gmail.com>
2025-06-02 14:40:27 +0530

    1

--- HashMap.cpp
+++ HashMap.cpp
@@ -47,19 +47,18 @@ public:
 };
 
 int main() {
-    // Test the HashMap implementation
     MyHashMap obj;
     obj.put(1, 10);
     obj.put(2, 20);
     
-    cout << "obj.get(1): " << obj.get(1) << endl;   // 10
-    cout << "obj.get(3): " << obj.get(3) << endl;   // -1
+    cout << "obj.get(1): " << obj.get(1) << endl;   
+    cout << "obj.get(3): " << obj.get(3) << endl;   
     
     obj.put(2, 30);
-    cout << "obj.get(2): " << obj.get(2) << endl;   // 30
+    cout << "obj.get(2): " << obj.get(2) << endl;   
     
     obj.remove(2);
-    cout << "obj.get(2): " << obj.get(2) << endl;   // -1
+    cout << "obj.get(2): " << obj.get(2) << endl;   
 
     return 0;
 }
